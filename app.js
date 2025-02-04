3dc24aef347469c452aa0fcbbd0761e46fedecd6
Chuchedark <viditonig@gmail.com>
2025-02-04 00:40:19 -0600

    Cambio total final codigo

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//https://trello.com/b/uYJFSK04/trello-challenge-amigo-secreto-esp
+
 let amigos = [];
 
-function agregarAmigos() {
-    let nombreAmigo = document.getElementById()
+function agregarAmigo() {
+    console.log(amigos);
+    let nombreAmigo = document.getElementById("");
+    return amigos.push(nombreAmigo);
+
+    
 }
+
+agregarAmigo();
